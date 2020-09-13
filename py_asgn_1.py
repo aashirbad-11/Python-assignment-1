dd0e00bcb9a9bc2051aa9c648858d30f7981aca7
Aashirbad Maharana <aashirbadmaharana11@gmail.com>
2020-09-13 22:03:22 +0530

    first asgmnt committed

---
+++ py_asgn_1.py
@@ -0,0 +1,25 @@
+'''
+1 Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, 
+between 2000 and 3200 (both included). The numbers obtained should be printed
+in a comma-separated sequence on a single line.
+'''
+for i in range(2000, 3201):
+	if (i % 7 == 0) and (i % 5 != 0):
+		print(i, end = ', ') 
+print('')
+'''
+2. Write a Python program to accept the user's first and last name and then getting 
+them printed in the the reverse order with a space between first name and last name.
+ '''
+first_name = input('Enter your first Name: ')
+last_name = input('Enter your last Name: ')
+print(first_name[: : -1] + ' ' + last_name[ : :-1])
+
+
+'''
+3. Write a Python program to find the volume of a sphere with diameter 12 cm.
+
+Formula: V=4/3 * π * r 3
+'''
+r = 12
+print('The volume is :', 4/3 * 3.14 * (r **3))
